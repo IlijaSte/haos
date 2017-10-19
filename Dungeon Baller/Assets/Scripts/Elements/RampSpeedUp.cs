680e441beb1890a6a23d29504059c6bfeb44a239
IlijaSte <ilijaste96@gmail.com>
2017-10-20 00:53:11 +0200

    Levels 1-7 finished
    for now

--- Dungeon Baller/Assets/Scripts/Elements/RampSpeedUp.cs
+++ Dungeon Baller/Assets/Scripts/Elements/RampSpeedUp.cs
@@ -4,19 +4,90 @@ using UnityEngine;
 
 public class RampSpeedUp : MonoBehaviour {
 
+	private static bool goingOver = false;
+	public bool goingOverThis = false;
+	public bool goingUp = false;
+	Rigidbody brb;
+	GameObject ball;
+	Transform brother = null;
+	private Vector3 startPos;
+	private static Vector3 startVel;
+
+	private Vector3 holdVelocity;
+	void Awake(){
+		ball = GameObject.Find ("Ball");
+		brb = ball.GetComponent<Rigidbody> ();
+		if (name == transform.parent.GetChild (0).name) {
+			brother = transform.parent.GetChild (1);
+		} else
+			brother = transform.parent.GetChild (0);
+	}
+
 	void OnTriggerEnter(Collider col){
 		if (col.gameObject.name == "Ball") {
 
-			Rigidbody brb = col.gameObject.GetComponent<Rigidbody> ();
+			if(!goingOver){
 
-			if (Mathf.Abs (brb.velocity.x) > Mathf.Abs (brb.velocity.z)) {
 
-				brb.velocity += new Vector3 (Mathf.Sign(brb.velocity.x) * 2, 0, 0);
-			} else {
 
-				brb.velocity += new Vector3 (0, 0, Mathf.Sign(brb.velocity.z) * 2);
+				float maxY = -35565;
+				Transform maxChild = transform;
+				foreach (Transform child in transform.parent) {
+					if (child.position.y > maxY) {
+						maxChild = child;
+						maxY = child.position.y;
+					}
+				}
+
+				if (transform == maxChild) {
+					goingUp = false;
+				} else {
+					goingUp = true;
+
+					goingOver = true;
+					goingOverThis = true;
+				}
+				print (goingUp);
+
 			}
 
 		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.gameObject.name == "Ball") {
+
+			if (goingOver && !goingOverThis) {
+
+				goingOver = false;
+
+			}
+
+		}
+	}
+
+	void FixedUpdate(){
+
+		if (!PlaySimulation.isSimActive) {
+			goingOverThis = false;
+			goingOver = false;
+			goingUp = false;
+		}
+		if (!goingOver) {
+			goingOverThis = false;
+			goingUp = false;
+		}
+		
+		if (goingOverThis && goingUp) {
+			float intensity = brb.mass * 9.81f * 0.447f + 0.5f;
+			if(Mathf.Abs(brb.velocity.x) > Mathf.Abs(brb.velocity.z)){
+				brb.AddForce (Mathf.Sign(brb.velocity.x) * intensity, intensity / 2.8f, 0, ForceMode.Force);
+			}else{
+				brb.AddForce (0, intensity / 2.8f, Mathf.Sign(brb.velocity.z) * intensity, ForceMode.Force);
+			}
+
+		}
+
+
 	}
 }
