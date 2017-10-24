93a9dc889c2071f3ab5f44d0672c9388257e8b6a
IlijaSte <ilijaste96@gmail.com>
2017-10-24 03:44:14 +0200

    haos

---
+++ Dungeon Baller/Assets/Scripts/Sound/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour {
+
+	float currentMusicTime;
+
+	void Start(){
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Update(){
+		currentMusicTime = GetComponent<AudioSource>().time;
+	}
+
+	void OnLevelWasLoaded(int lvl){
+		GetComponent<AudioSource>().time = currentMusicTime;
+	}
+}
