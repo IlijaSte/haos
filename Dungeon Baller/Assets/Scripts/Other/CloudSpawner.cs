01a24b3041ee4c793ef8eb1fbf5726a2138d53da
IlijaSte <ilijaste96@gmail.com>
2017-12-23 05:41:24 +0100

    new clouds impl

--- Dungeon Baller/Assets/Scripts/Other/CloudSpawner.cs
+++ Dungeon Baller/Assets/Scripts/Other/CloudSpawner.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CloudSpawner : MonoBehaviour {
 
-	public GameObject cloudPrefab;
+	[SerializeField]
+	public GameObject[] cloudPrefabs;
 	private float i = 0;
 	public Transform cloudSpace;
 	public float spawnSpeed = 0;
@@ -22,7 +23,7 @@ public class CloudSpawner : MonoBehaviour {
 			j++;
 		}
 		for (int i = 0; i < j; i++) {
-			GameObject newCloud = Instantiate (cloudPrefab);
+			GameObject newCloud = Instantiate (cloudPrefabs[Random.Range(0, cloudPrefabs.Length - 1)]);
 
 			Vector3 x = Quaternion.AngleAxis (cloudSpace.rotation.eulerAngles.y, Vector3.up) * new Vector3 (Random.Range (0, cloudSpace.localScale.x) - cloudSpace.localScale.x / 2, 0, 0);
 
@@ -31,11 +32,11 @@ public class CloudSpawner : MonoBehaviour {
 
 			newCloud.transform.position = Vector3.Lerp(newCloud.transform.position, ((Random.Range(0, 2) == 0) ? newCloud.transform.position + z:newCloud.transform.position - z), Random.Range(0f, 1f));
 			newCloud.transform.position = new Vector3 (newCloud.transform.position.x, cloudSpace.position.y + Random.Range (0, cloudSpace.localScale.y) - cloudSpace.localScale.y / 2, newCloud.transform.position.z);
-			newCloud.transform.localScale = new Vector3 (Random.Range (8, 12) * 5, Random.Range (1, 3) * 5, Random.Range (6, 8) * 5);
-			newCloud.transform.rotation = cloudSpace.rotation;
+			//newCloud.transform.localScale = new Vector3 (Random.Range (8, 12) * 5, Random.Range (1, 3) * 5, Random.Range (6, 8) * 5);
+			newCloud.transform.rotation = Quaternion.Euler(cloudSpace.rotation.eulerAngles + new Vector3(0, 90, 0));
 			float cloudSpeed = Random.Range (10f, 15f);
 
-			newCloud.GetComponent<CloudMovement> ().speed = new Vector3(cloudSpeed, 0, 0);
+			newCloud.GetComponent<CloudMovement> ().speed = new Vector3(0, 0, cloudSpeed);
 			//newCloud.GetComponent<CloudMovement> ().speed = new Vector3(x1, 0, y1);
 		}
 
@@ -48,22 +49,22 @@ public class CloudSpawner : MonoBehaviour {
 		if (i >= 25) {
 
 
-			GameObject newCloud = Instantiate (cloudPrefab);
+			GameObject newCloud = Instantiate (cloudPrefabs[Random.Range(0, cloudPrefabs.Length - 1)]);
 
-			Vector3 x = Quaternion.AngleAxis (cloudSpace.rotation.eulerAngles.y, Vector3.up) * new Vector3 (Random.Range (0, cloudSpace.localScale.x) - cloudSpace.localScale.x / 2, 0, 0);
+			Vector3 x = Quaternion.AngleAxis (cloudSpace.rotation.eulerAngles.y, Vector3.up) * new Vector3 (cloudSpace.localScale.x / 2, 0, 0);
 
-			newCloud.transform.position = Vector3.Lerp (cloudSpace.position, ((Random.Range (0, 1) == 0) ? cloudSpace.position + x : cloudSpace.position - x), Random.Range (0f, 1f));
+			newCloud.transform.position = cloudSpace.position - x;
 			Vector3 z = Quaternion.AngleAxis (cloudSpace.rotation.eulerAngles.y, Vector3.up) * new Vector3 (0, 0, Random.Range (0, cloudSpace.localScale.z) - cloudSpace.localScale.z / 2);
 
 			newCloud.transform.position = Vector3.Lerp(newCloud.transform.position, ((Random.Range(0, 1) == 0) ? newCloud.transform.position + z:newCloud.transform.position - z), Random.Range(0f, 1f));
 			newCloud.transform.position = new Vector3 (newCloud.transform.position.x, cloudSpace.position.y + Random.Range (0, cloudSpace.localScale.y) - cloudSpace.localScale.y / 2, newCloud.transform.position.z);
-			newCloud.transform.localScale = new Vector3 (Random.Range (8, 12) * 5, Random.Range (1, 3) * 5, Random.Range (6, 8) * 5);
+			//newCloud.transform.localScale = new Vector3 (Random.Range (8, 12) * 5, Random.Range (1, 3) * 5, Random.Range (6, 8) * 5);
 			newCloud.transform.rotation = cloudSpace.rotation;
 			float cloudSpeed = Random.Range (5f, 8f);
 
 			newCloud.GetComponent<CloudMovement> ().speed = new Vector3(cloudSpeed, 0, 0);
 			//newCloud.GetComponent<CloudMovement> ().speed = new Vector3(x1, 0, y1);
-			spawnSpeed = Random.Range(5, 9);
+			spawnSpeed = Random.Range(2, 4);
 			i = 0f;
 		}
 
